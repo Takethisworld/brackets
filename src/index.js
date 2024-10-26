3838276c9d3fc0e7a5341c23e3931b9f27ec1551
Zaur Saidov <saidov199102@icloud.com>
2024-10-27 03:59:02 +0600

    resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const stack = [];
+  const bracketPairs={};
+  const openBrackets = new Set();
+
+bracketsConfig.map(([open,close])=>{
+  bracketPairs[close]=open;
+  openBrackets.add(open)
+});
+
+for(item of str){
+  const isOpening = openBrackets.has(item);
+  const isClosing = item in bracketPairs;
+
+  if(isOpening && (!isClosing || stack[stack.length - 1] !== item)){
+    stack.push(item);
+  }else if(isClosing){
+    if(stack.pop() !== bracketPairs[item]){
+      return false
+    }
+  }
+}
+  return stack.length === 0;
 }
